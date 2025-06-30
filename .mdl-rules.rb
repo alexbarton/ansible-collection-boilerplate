21e4ca09b5b05a37176bfd636b97bc6293143a7b
Alexander Barton <alex@barton.de>
2025-06-30 16:56:51 +0200

    Update mdl "rules file" to current syntax

--- .mdl-rules.rb
+++ .mdl-rules.rb
@@ -2,5 +2,5 @@
 # See <https://github.com/markdownlint/markdownlint/blob/main/docs/creating_rules.md>
 
 all
-rule 'MD013', :code_blocks => false
+rule 'MD013', :ignore_code_blocks => true
 rule 'MD029', :style => :ordered
